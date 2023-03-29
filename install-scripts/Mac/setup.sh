58f3c3ae6d77d0f611f0e1a730fd80b1bbb784e7
nicohash <58517269+nicohash@users.noreply.github.com>
2023-03-30 00:34:38 +0200

    add install script for Mac

---
+++ install-scripts/Mac/setup.sh
@@ -0,0 +1,49 @@
+#==============
+# Install all the packages
+#==============
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew doctor
+brew update
+
+# So we use all of the packages we are about to install
+echo "export PATH='/usr/local/bin:$PATH'\n" >> ~/.bashrc
+source ~/.bashrc
+
+#==============
+# Remove old dot flies
+#==============
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+sudo rm -rf ~/.p10k.zsh > /dev/null 2>&1
+sudo rm -rf ~/Brewfile > /dev/null 2>&1
+
+#==============
+# Create symlinks in the home folder
+#==============
+SYMLINKS=()
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+SYMLINKS+=('.zshrc')
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+SYMLINKS+=('.gitconfig')
+ln -s ~/dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+SYMLINKS+=('.p10k.zsh')
+ln -sf ~/dotfiles/homebrew/Brewfile ~/Brewfile
+SYMLINKS+=('Brewfile')
+
+echo ${SYMLINKS[@]}
+
+cd ~
+brew bundle
+cd -
+
+#==============
+# Set zsh as the default shell
+#==============
+chsh -s /bin/zsh
+
+#==============
+# And we are done
+#==============
+echo -e "\n====== All Done!! ======\n"
+echo
+echo "Enjoy"
