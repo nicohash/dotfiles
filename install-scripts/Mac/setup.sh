27fcf81442affa3e07279db035a9a4db19e262bb
nicohash <58517269+nicohash@users.noreply.github.com>
2023-03-31 19:55:28 +0200

    fix setup.sh

--- install-scripts/Mac/setup.sh
+++ install-scripts/Mac/setup.sh
@@ -1,9 +1,7 @@
 #==============
 # Install all the packages
 #==============
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-sudo chown -R $(whoami):admin /usr/local
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew doctor
 brew update
 
