aea69ce26ff575482506dbd363dddde620b2f99e
nicohash <58517269+nicohash@users.noreply.github.com>
2023-04-07 18:26:33 +0200

    remove dock setup from setup.sh

--- scripts/Mac/setup.sh
+++ scripts/Mac/setup.sh
@@ -1,3 +1,6 @@
+# Import helpers
+source "$HOME/dotfiles/scripts/Mac/helpers/style.sh"
+
 #==============
 # Install developer tools
 #==============
@@ -47,47 +50,6 @@ echo ${SYMLINKS[@]}
 cd ~
 brew bundle
 cd -
-
-#==============
-# Configure Dock
-#==============
-echo "$(tput bold)Configuring Dock$(tput sgr0)"
-
-defaults write com.apple.dock autohide -bool true
-echo "Autohide active"
-defaults write com.apple.dock autohide-delay -float 0.2
-echo "Autohide delay: 0.2 seconds"
-defaults write com.apple.dock autohide-time-modifier -float 0.2
-echo "Autohide time modifier: 0.2 seconds"
-defaults write com.apple.dock mineffect -string "scale"
-echo "Minimize effect: scale"
-defaults write com.apple.dock show-recents -bool false
-echo "Recent applications hidden"
-
-read -p "Dock spacers (press $(tput bold)ENTER$(tput sgr0) to skip): " spacers
-spacers=${spacers:-0}
-
-if [[ $spacers =~ ^[0-9]+$ ]]; then
-        addedSpacers=0
-
-        while [[ $spacers > 0 ]]
-        do
-                defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="small-spacer-tile";}'
-                ((spacers--))
-                ((addedSpacers++))        
-        done
-
-        echo "Spacers added: $addedSpacers"
-else
-        echo "Input not allowed"
-        echo "Add small spacers manually: defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="small-spacer-tile";}' && killall Dock"
-fi
-
-
-# restarting Dock
-killall Dock
-
-echo "$(tput bold)Dock configured$(tput sgr0)"
 
 #==============
 # Set zsh as the default shell
