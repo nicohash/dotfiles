9278694c07df8d666310806a8413bfe4b4d54b7f
nicohash <58517269+nicohash@users.noreply.github.com>
2023-05-21 17:09:03 +0200

    refactor scripts for Linux into setup.sh

--- scripts/Linux/setup.sh
+++ scripts/Linux/setup.sh
@@ -7,10 +7,39 @@
 #===============================================================================
 
 #==============
+# Import helpers
+#==============
+source "$HOME/dotfiles/scripts/helpers/style.sh"
+
+#==============
 # Variables
 #==============
 dotfiles_dir=~/dotfiles
 log_file=~/install_progress_log.txt
+
+#==============
+# Install Packages
+#==============
+sudo apt-get -y install zsh
+if type -p zsh > /dev/null; then                                                                                                                                                          
+    echo "$(bold "zsh") installed" >> $log_file
+else
+    echo "$(bold "zsh") FAILED to install!" >> $log_file
+fi
+
+sudo apt-get install zsh-syntax-highlighting
+sudo apt-get install zsh-autosuggestions
+
+sudo apt-get -y install vim
+if type -p vim > /dev/null; then
+    echo "$(bold "Vim") installed" >> $log_file
+else
+    echo "$(bold "Vim") FAILED to install!" >> $log_file
+fi
+
+git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+# not sure if this is necessary:
+# echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 #==============
 # Delete existing dot files and folders
@@ -21,17 +50,14 @@ sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.p10k.zsh > /dev/null 2>&1
 sudo rm -rf ~/.gitconfig > /dev/null 2>&1
 
-
 #==============
 # Create symlinks in the home folder
-# Allow overriding with files of matching names in the custom-configs dir
 #==============
 ln -s $dotfiles_dir/vim ~/.vim
 ln -s $dotfiles_dir/vim/vimrc ~/.vimrc
 ln -sf $dotfiles_dir/zsh/zshrc ~/.zshrc
 ln -sf $dotfiles_dir/zsh/p10k.zsh ~/.p10k.zsh
-# maybe -sf (force) is better instead of -s
-ln -s $dotfiles_dir/gitconfig ~/.gitconfig
+ln -sf $dotfiles_dir/gitconfig ~/.gitconfig
 
 #==============
 # Set zsh as the default shell
@@ -41,7 +67,7 @@ sudo chsh -s /bin/zsh
 #==============
 # Give the user a summary of what has been installed
 #==============
-echo -e "\n====== Summary ======\n"
+echo -e "\n$(bold "====== Summary ======")\n"
 cat $log_file
 echo
 echo "Enjoy"
